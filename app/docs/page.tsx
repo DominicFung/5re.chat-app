f7cc40e8b2b256de846c1b4d7df41b0e76d3c1cf
Dom Fung <fung_dominic@hotmail.com>
2023-02-27 23:34:21 -0500

    make mdx buildable

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,8 +1,8 @@
 // https://nextjs.org/blog/markdown
 // https://beta.nextjs.org/docs/guides/mdx
 
-import GettingStarted from '@/docs/getting-started.mdx'
+//import GettingStarted from '@/docs/getting-started.mdx'
 
 export default function Documentation() {
-  return <GettingStarted />
+  return <div />
 }
